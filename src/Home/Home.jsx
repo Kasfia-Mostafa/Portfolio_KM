713d4f8ceefb2db532d5bee9ea60def6cf2fe4c9
SwornaCode <kasfiamostafa03@gmail.com>
2023-12-12 16:21:23 +0600

    img and education

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,12 +4,14 @@ import Footer from "../Footer/Footer";
 import Project from "../Projects/Project";
 import Skill from "../Skill/Skill";
 import Banner from "../Banner/Banner";
+import Edu from "../Edu/Edu";
 
 const Home = () => {
   return (
     <div className="bg-[#fcdac1] max-w-full max-h-screen font-DM">
       <Banner></Banner>
       <About></About>
+      <Edu></Edu>
       <Skill></Skill>
       <Project></Project>
       <Contact></Contact>
--- src/Skill/Skill.jsx
+++ src/Skill/Skill.jsx
@@ -20,90 +20,90 @@ const Skill = () => {
         <Marquee pauseOnHover={true} speed={100}>
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/HTML.svg"
+            src="https://i.ibb.co/h96Wzy2/html5-3384014-1280.png"
             alt=""
           />
-          <img className="w-20 mx-14" src="/src/assets/Skills/CSS.svg" alt="" />
+          <img className="w-20 mx-14" src="https://i.ibb.co/JsPn4nk/css.jpg" alt="" />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/TailwindCSS-Light.svg"
-            alt=""
-          />
-          <img
-            className="w-20 mx-14"
-            src="/src/assets/Skills/JavaScript.svg"
+            src="https://i.ibb.co/dWF8Kmm/3542736-21cf-1024x573.jpg"
             alt=""
           />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/React-Light.svg"
+            src="https://i.ibb.co/GnNdTJj/js.jpg"
             alt=""
           />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/MongoDB.svg"
+            src="https://i.ibb.co/nndPctC/th.jpg"
             alt=""
           />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/NodeJS-Light.svg"
+            src="https://i.ibb.co/ZHS9dCm/1-WSP9-OMy-Xb-ULdf-Zmx8-t6-Vw.jpg"
             alt=""
           />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/ExpressJS-Light.svg"
+            src="https://i.ibb.co/p0Nm0Dp/nd.png"
             alt=""
           />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/Firebase-Light.svg"
+            src="https://i.ibb.co/FmcPdLh/expressjsframework.webp"
             alt=""
           />
-          <img className="w-20 mx-14" src="/src/assets/Skills/Git.svg" alt="" />
+          <img
+            className="w-20 mx-14"
+            src="https://i.ibb.co/1TvTsFk/f.jpg"
+            alt=""
+          />
+          <img className="w-20 mx-14" src="https://i.ibb.co/cFyLRqB/git.jpg" alt="" />
         </Marquee>
         <Marquee pauseOnHover={true} speed={100}>
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/HTML.svg"
+            src="https://i.ibb.co/h96Wzy2/html5-3384014-1280.png"
             alt=""
           />
-          <img className="w-20 mx-14" src="/src/assets/Skills/CSS.svg" alt="" />
+           <img className="w-20 mx-14" src="https://i.ibb.co/JsPn4nk/css.jpg" alt="" />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/TailwindCSS-Light.svg"
+            src="https://i.ibb.co/dWF8Kmm/3542736-21cf-1024x573.jpg"
             alt=""
           />
-          <img className="w-20 mx-14" src="/src/assets/Skills/Git.svg" alt="" />
+         <img className="w-20 mx-14" src="https://i.ibb.co/cFyLRqB/git.jpg" alt="" />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/MongoDB.svg"
+            src="https://i.ibb.co/ZHS9dCm/1-WSP9-OMy-Xb-ULdf-Zmx8-t6-Vw.jpg"
             alt=""
           />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/JavaScript.svg"
+            src="https://i.ibb.co/GnNdTJj/js.jpg"
             alt=""
           />
            <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/ExpressJS-Light.svg"
+            src="https://i.ibb.co/FmcPdLh/expressjsframework.webp"
             alt=""
           />
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/React-Light.svg"
+            src="https://i.ibb.co/nndPctC/th.jpg"
             alt=""
           />
           
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/NodeJS-Light.svg"
+            src="https://i.ibb.co/p0Nm0Dp/nd.png"
             alt=""
           />
          
           <img
             className="w-20 mx-14"
-            src="/src/assets/Skills/Firebase-Light.svg"
+            src="https://i.ibb.co/1TvTsFk/f.jpg"
             alt=""
           />
           
